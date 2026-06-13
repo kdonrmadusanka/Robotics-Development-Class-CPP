fec13a9584e64d564140626f235e269f10d6ebaa
kdonrmadusanka <ravmkdon@gmail.com>
2026-06-13 17:01:17 +0530

    Added the Compilation process as a comment.

--- Class1/compilation/main.cpp
+++ Class1/compilation/main.cpp
@@ -5,3 +5,12 @@ int main()
 {
     std::cout << "I love build systems.\n";
 }
+
+
+/* Compilation Process
+ * 1. g++ -E main.cpp < main.i
+ * 2. g++ -S main.i
+ * 3. g++ -c main.s
+ * 4. g++ main.o -o main
+ * 5. ./main
+ */
